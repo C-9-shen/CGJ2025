ef6de9b4e16a19b13a689cf523dd8430fa31a135
9_shen <93798932+C-9-shen@users.noreply.github.com>
2025-06-28 16:24:19 +0800

    Upload Part of image

--- Assets/Scripts/Utils/AnythingDebug.cs
+++ Assets/Scripts/Utils/AnythingDebug.cs
@@ -19,9 +19,9 @@ public class AnythingDebug : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.D)) transform.position += new Vector3(speed, 0, 0);
-        if(Input.GetKey(KeyCode.A)) transform.position -= new Vector3(speed, 0, 0);
-        if(Input.GetKey(KeyCode.W)) transform.position += new Vector3(0, speed, 0);
-        if(Input.GetKey(KeyCode.S)) transform.position -= new Vector3(0, speed, 0);
+        if(Input.GetKey(KeyCode.D)) transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.A)) transform.position -= new Vector3(speed*Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.W)) transform.position += new Vector3(0, speed*Time.deltaTime, 0);
+        if(Input.GetKey(KeyCode.S)) transform.position -= new Vector3(0, speed*Time.deltaTime, 0);
     }
 }
